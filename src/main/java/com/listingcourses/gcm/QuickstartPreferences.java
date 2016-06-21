d606848157ba2153f43bd40e3d17796febb3416e
Janneau Julien <julien.janneau@gmail.com>
2016-06-21 19:18:06 +0200

    Initial commit

---
+++ src/main/java/com/listingcourses/gcm/QuickstartPreferences.java
@@ -0,0 +1,12 @@
+package com.listingcourses.gcm;
+
+/**
+ * Created by julien on 21/06/2016.
+ */
+public class QuickstartPreferences {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String FCM_TOKEN = "FCMToken";
+
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+}
---
+++ src/main/java/com/listingcourses/gcm/RegistrationIntentService.java
@@ -0,0 +1,43 @@
+package com.listingcourses.gcm;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GcmPubSub;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+import java.io.IOException;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class RegistrationIntentService extends IntentService {
+
+    private static final String TAG = "RegIntentService";
+
+    public RegistrationIntentService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        try {
+            InstanceID instanceID = InstanceID.getInstance(this);
+            String token = instanceID.getToken(getString(R.string.project_id), GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+
+        } catch (Exception e) {
+            Log.d(TAG, "Failed to complete token refresh", e);
+        }
+
+    }
+}
